699efbb1d62bb6921bdfce2c4038c579766199a0
maki-yoshiuchi <yoshiuchi_maki@lightcafe.co.jp>
2023-08-03 11:01:12 +0900

    jQueryオブジェクトの変数名に$をつける

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(function() {
-  const zipcode = $('.js-zipcode');
-  const searchButton = $('.js-search-button');
-  const address = $('.js-address');
+  const $zipcode = $('.js-zipcode');
+  const $searchButton = $('.js-search-button');
+  const $address = $('.js-address');
   let inputZipcode;
 
-  zipcode.on('input', function() {
-    address.val('');
+  $zipcode.on('input', function() {
+    $address.val('');
     inputZipcode = $(this).val();
     $(this).val(inputZipcode.replace(/[^0-9]/g, ''));
     const isValidInput = $.isNumeric(inputZipcode) && inputZipcode.length === 7;
-    searchButton.prop('disabled', !isValidInput);
+    $searchButton.prop('disabled', !isValidInput);
   });
 
-  searchButton.on('click', function() {
+  $searchButton.on('click', function() {
     $.ajax({
       url: `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${inputZipcode}`,
       type: 'GET',
@@ -20,7 +20,7 @@ $(function() {
       const response = JSON.parse(data).results;
       if (response) {
         const [{address1, address2, address3}] = response;
-        address.val(address1 + address2 + address3);
+        $address.val(address1 + address2 + address3);
       } else {
         alert('その郵便番号は存在しません');
       }
